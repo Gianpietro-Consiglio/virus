a3335f569ab5305c93fd965900950eb43e3ad394
Gianpietro Consiglio <83292980+Gianpietro-Consiglio@users.noreply.github.com>
2022-03-01 20:54:09 -0300

    1.0

---
+++ virus.py
@@ -0,0 +1,23 @@
+import os
+import pyscreenshot
+import webbrowser as wb
+
+cont = 0
+while True:
+    cont += 1
+    try:
+        #os.mkdir(f"C:\\arquivo{cont}")
+        #os.chdir(f"C:\\arquivo{cont}")
+        print(f"Processo -> {cont}")
+        if cont == 15:
+            break
+       
+    except:
+        pass
+    
+    else:
+        wb.open('https://baidu-antivirus.br.uptodown.com/windows/download')
+        #image = pyscreenshot.grab(bbox=(10, 10, 50, 50))
+        #image.show()
+        continue
+    
